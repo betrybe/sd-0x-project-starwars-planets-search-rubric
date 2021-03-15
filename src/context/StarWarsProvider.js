b4acfd623a6c25b50a22da866020138cbe08eec3
LizzardMedeiros <luis.medeiros@outlook.com.br>
2021-03-15 11:58:06 -0300

    Have to pass

--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -80,7 +80,7 @@ function StarWarsProvider({ children }) {
           Planet,
           filter.column,
           filter.comparison,
-          filter.value, filter,
+          filter.value,
         )
       ))));
     };
